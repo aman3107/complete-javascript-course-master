716c95da9925d520560d2a5b11929e59eede8072
Aman Mittal <amanmittalma@gmail.com>
2023-06-11 22:44:17 +0530

    JavaScript

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -108,34 +108,194 @@ let peron = "Aman Mittal";
 
 /* Basic Operators */
 
-const now = 2037;
-const ageAman = now - 1999;
-const ageSarah = now - 2018;
-console.log(ageAman);
-console.log(ageSarah);
-console.log(ageAman * 2);
-console.log(ageAman / 2, 2 ** 3); // 2**3 means 2 to the power 3 = 2 * 2 * 2
+// const now = 2037;
+// const ageAman = now - 1999;
+// const ageSarah = now - 2018;
+// console.log(ageAman);
+// console.log(ageSarah);
+// console.log(ageAman * 2);
+// console.log(ageAman / 2, 2 ** 3); // 2**3 means 2 to the power 3 = 2 * 2 * 2
 
-const fName = "Aman";
-const lName = "Mittal";
-console.log(fName + " " + lName);
+// const fName = "Aman";
+// const lName = "Mittal";
+// console.log(fName + " " + lName);
 
-let x = 10 + 5; // 15
-x += 10; // x = x + 10;
-x *= 4;
-x++;
-x--;
-console.log(x);
+// let x = 10 + 5; // 15
+// x += 10; // x = x + 10;
+// x *= 4;
+// x++;
+// x--;
+// console.log(x);
 
-console.log(ageAman > ageSarah);
-console.log(ageSarah >= 18);
+// console.log(ageAman > ageSarah);
+// console.log(ageSarah >= 18);
 
-const isFullAge = ageSarah >= 18;
+// const isFullAge = ageSarah >= 18;
 
-n = 28;
-if(n % 10 == 0){
-    console.log('Multiple of 10');
-}
-else{
-    console.log('Not Multiple of 10');
-}
+/* Operator Precedene */
+
+// console.log(25-10-5);
+// let x, y;
+// x = y = 25-10-5;
+// console.log(x,y)
+// const now = 2037;
+// const ageAman = now - 1999;
+// const ageSarah = now - 2019;
+// const avgAge = (ageAman + ageSarah) / 2;
+// console.log(avgAge);
+
+/* Strings and Template Literals */
+
+// const firstName = 'Aman';
+// const job = 'Enginner';
+// const birthYear = 1999;
+// const year = 2023;
+
+// const aman = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job;
+
+// console.log(aman);
+
+// const amanNew = `I'm ${firstName}, a ${year-birthYear} years old ${job}`;
+// console.log(amanNew);
+
+// console.log(`Just a regular string ...`);
+// console.log(`String with
+// Multiple
+// Lines`)
+
+/* IF / ELSE Statements */
+
+// const age = 15;
+// const isOldEnough = age >= 18;
+// console.log(isOldEnough);
+
+// if (age >= 18) {
+//   console.log("Sarah can start driving license 🚗");
+// }
+// else{
+//     const yearsLeft = 18 - age;
+//     console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`)
+// }
+
+// const birthYear = 2021;
+// let century;
+// if(birthYear <= 2000){
+//     century = 20;
+// }
+// else{
+//     century = 21;
+// }
+
+// console.log(century);
+
+/* Type Conversion and Coercion */
+
+// type conversion
+
+// const inputYear = '1999';
+// console.log(Number(inputYear), inputYear);
+// console.log(Number(inputYear) + 18);
+
+// console.log(Number('Aman'));
+// console.log(typeof NaN);
+
+// console.log(String(23) + 20);
+
+// type Coercion
+
+// console.log('I am ' + 23 + ' years old')
+// console.log('23' - 10 - 3);
+// console.log('23' * 2);
+// let n = 1 + '1'; // '11'
+// n = n - 1; // 11 - 1 = 10
+// console.log(n);
+
+/* Truthy and Falsy Values */
+
+// 5 falsy values: 0, '', undefined, null, NaN
+
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean('Aman'));
+// console.log(Boolean({}));
+// console.log(Boolean(''));
+
+// let money = 0;
+
+// if(money){
+//     console.log("Don't spend it all");
+// }
+// else{
+//     console.log("You should get a job");
+// }
+
+// let height;
+// if(height){
+//     console.log("YaY! Height is defined");
+// }
+// else{
+//     console.log("Height is undefined");
+// }
+
+/* Equality Operators : == vs === */
+
+// const age = '18'; 
+
+// if(age == 18){
+//     console.log('You just became an adult (loose)');
+// }
+
+// if(age === 18){
+//     console.log('You just became an adult (strict)');
+// }
+// console.log(18 == '18');
+// console.log(18 === '18');
+
+// const favoriteNumber = Number(prompt("What's your favorite number"));
+// console.log(favoriteNumber);
+// console.log(typeof favoriteNumber);
+
+// if(favoriteNumber == 23){
+//     console.log('Cool 23 is an amazing number (loose)');
+// }
+
+// if(favoriteNumber === 23){
+//     console.log('Cool 23 is an amazing number (strict)');
+// }
+// else if(favoriteNumber === 7){
+//     console.log('7 is also a cool number');
+// }
+// else{
+//     console.log('Number is not 23 or 7');   
+// }
+
+/* Logical Operators */
+
+// const hasDriversLicense = true;
+// const hasGoodVision = true;
+
+// console.log(hasDriversLicense && hasGoodVision);
+// console.log(hasDriversLicense || hasGoodVision);
+// console.log(!hasDriversLicense)
+
+// const shoulDrive = hasDriversLicense && hasGoodVision;
+
+// if(hasDriversLicense && hasGoodVision){
+//     console.log('Sarah is able to drive');
+// }
+// else{
+//     console.log('Someone should have drive');
+// }
+
+// const isTired = true;
+// console.log(hasDriversLicense || hasGoodVision || isTired);
+// console.log(hasDriversLicense && hasGoodVision && isTired);
+
+// if(hasDriversLicense && hasGoodVision && !isTired){
+//     console.log('Sarah is able to drive');
+// }
+// else{
+//     console.log('Someone should have drive');
+// }
+
+
