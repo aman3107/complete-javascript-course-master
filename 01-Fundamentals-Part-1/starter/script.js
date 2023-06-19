d2fc8b46faaa06b04b3e0de76783e547b5f0c0f8
Aman Mittal <amanmittalma@gmail.com>
2023-06-19 14:09:11 +0530

    JavaScript Fundamentals

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -723,3 +723,23 @@ let peron = "Aman Mittal";
 // console.log(max);
 
 
+function sumDigit(n){
+    let sum = 0;
+    for(let i = n; i > 0; i = Math.floor(i / 10)){
+        let rem = i % 10;
+        sum = sum + rem;
+    }
+    return sum;
+}
+console.log(sumDigit(24681));
+
+
+function sumDigit(n){
+    let sum = 0;
+    for(let i = n; i > 0; i = (i / 10)|0){
+        let rem = i % 10;
+        sum = sum + rem;
+    }
+    return sum;
+}
+console.log(sumDigit(24681));
