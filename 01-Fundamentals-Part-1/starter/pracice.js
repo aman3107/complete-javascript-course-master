b196537e10da71a91342cb99bb0404832c22bac9
Aman Mittal <amittal@mirrahealthcare.com>
2023-12-23 23:06:54 +0530

    Practice

---
+++ 01-Fundamentals-Part-1/starter/pracice.js
@@ -0,0 +1,46 @@
+// console.log("Hello World");
+// alert("Hello World");
+
+// let js = "Amazing";
+// if (js == "Amazing") {
+//   alert("JavaScript is fun!");
+// }
+// console.log("Aman Mittal");
+// console.log("How are you");
+
+/* Values and Variables */
+
+// console.log("Jonas");
+// console.log(23);
+
+// let firstName = "Payal";
+// console.log(firstName);
+
+// let PI = 3.1415;
+
+// let myFirstJob = "Programmer";
+// let myCurrentJob = "Teacher";
+
+/* Data Types */
+
+// let age = 23;
+// let firstName = "Aman";
+// let fullAge = true;
+// let children;
+// console.log(age, firstName, fullAge, children);
+// console.log(typeof age);
+// console.log(typeof firstName);
+// console.log(typeof fullAge);
+// console.log(typeof children);
+
+// fullAge = "Papa";
+// console.log(fullAge);
+
+/* let, const and var */
+
+const birthYear = 1999;
+// birthYear = 2000; Giving Error
+// const job;  Giving Error
+
+var job = "Programmer";
+job = "Teacher";
