bcec15d5f43f0dc899ce664463b7bde61dd87418
Aman Mittal <amittal@mirrahealthcare.com>
2024-01-02 22:04:46 +0530

    Adding min

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -6,17 +6,20 @@ const x = 23;
 const temprature = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
 const temprature2 = [3, -2, -6, -1, "error", 9, 13, 17, 17, 14, 9, 5];
 
-let max = temprature[0];
-
 const calcTempAmplitude = function (temprature) {
+  let max = temprature[0];
+  let min = temprature[0];
   for (let i = 1; i < temprature.length; i++) {
     if (temprature[i] === "error") {
       continue;
     } else if (temprature[i] > max) {
       max = temprature[i];
+    } else if (temprature[i] < min) {
+      min = temprature[i];
     }
   }
-  return max;
+  let ans = [max, min];
+  return ans;
 };
 
 console.log(calcTempAmplitude(temprature));
