a7ec58403fc4b249e565ea2f7c6c45f51351e846
Aman Mittal <amittal@mirrahealthcare.com>
2024-01-02 21:59:56 +0530

    Calculate Temp Amplitude

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -2,3 +2,19 @@
 "use strict";
 
 const x = 23;
+
+const temprature = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+let max = temprature[0];
+
+const calcTempAmplitude = function (temprature) {
+  for (let i = 1; i < temprature.length; i++) {
+    if (temprature[i] === "error") {
+      continue;
+    } else if (temprature[i] > max) {
+      max = temprature[i];
+    }
+  }
+  return max;
+};
+
+console.log(calcTempAmplitude(temprature));
