0563e809f41f6105708ef1feaa167285d6d8118a
Aman Mittal <amittal@mirrahealthcare.com>
2024-01-04 22:47:38 +0530

    DOM and DOM Manipulation, query Selector

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,4 +1,11 @@
 'use strict';
 
-document.querySelector('.message');
 console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
+
+document.querySelector('.guess').value = 20;
+console.log(document.querySelector('.guess').value);
