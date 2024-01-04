3fec0251a15493e9fe26ed7fc9358b68d5ff4612
Aman Mittal <amittal@mirrahealthcare.com>
2024-01-04 23:29:57 +0530

    addEvenListener

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = 'Correct Number';
 console.log(document.querySelector('.message').textContent);
@@ -9,3 +9,15 @@ document.querySelector('.score').textContent = 20;
 
 document.querySelector('.guess').value = 20;
 console.log(document.querySelector('.guess').value);
+*/
+
+document.querySelector('.check').addEventListener('click', function () {
+  var guessNumber = Number(document.querySelector('.guess').value);
+  console.log(guessNumber, typeof guessNumber);
+
+  if (!guessNumber) {
+    document.querySelector('.message').textContent = '⛔ No Number';
+  }
+  // document.querySelector('.message').textContent = 'Correct Number!';
+  // document.querySelector('.score').textContent = x;
+});
