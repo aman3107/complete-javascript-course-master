ce315ac8d9dc38a23675eb9c6ed01dcf0c98a6ec
Aman Mittal <amittal@mirrahealthcare.com>
2024-01-06 00:01:32 +0530

    Guess My Number Project

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -11,13 +11,48 @@ document.querySelector('.guess').value = 20;
 console.log(document.querySelector('.guess').value);
 */
 
-document.querySelector('.check').addEventListener('click', function () {
-  var guessNumber = Number(document.querySelector('.guess').value);
-  console.log(guessNumber, typeof guessNumber);
+// document.querySelector('.check').addEventListener('click', function () {
+//   var guessNumber = Number(document.querySelector('.guess').value);
+//   console.log(guessNumber, typeof guessNumber);
 
-  if (!guessNumber) {
+//   if (!guessNumber) {
+//     document.querySelector('.message').textContent = '⛔ No Number';
+//   }
+//   document.querySelector('.message').textContent = 'Correct Number!';
+//   document.querySelector('.score').textContent = x;
+// });
+
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+console.log(secretNumber);
+document.querySelector('.number').textContent = secretNumber;
+
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  if (!guess) {
     document.querySelector('.message').textContent = '⛔ No Number';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too High!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too Low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
   }
-  // document.querySelector('.message').textContent = 'Correct Number!';
-  // document.querySelector('.score').textContent = x;
 });
