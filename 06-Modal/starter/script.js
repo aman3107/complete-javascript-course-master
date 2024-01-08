30c411d3b98fd765857cc7a9edb4bce7993c83d0
Aman Mittal <amittal@mirrahealthcare.com>
2024-01-08 20:02:53 +0530

    modal window project

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,37 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsShowModal.length; i++) {
+  // console.log(btnsShowModal[i].textContent);
+  // btnsShowModal[i].addEventListener('click', function () {
+  //   modal.classList.remove('hidden');
+  //   overlay.classList.remove('hidden');
+  //   btnCloseModal.addEventListener('click', function () {
+  //     modal.classList.add('hidden');
+  //     overlay.classList.add('hidden');
+  //   });
+  // });
+  btnsShowModal[i].addEventListener('click', openModal);
+}
+btnCloseModal.addEventListener('click', closeModal);
+
+// overlay.addEventListener('click', function () {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// });
+
+overlay.addEventListener('click', closeModal);
