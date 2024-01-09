325ac2e572c0610d477367b64a1b878d40f49984
Aman Mittal <amittal@mirrahealthcare.com>
2024-01-09 16:29:38 +0530

    Modal Window Project Complete

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -35,3 +35,8 @@ btnCloseModal.addEventListener('click', closeModal);
 // });
 
 overlay.addEventListener('click', closeModal);
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
