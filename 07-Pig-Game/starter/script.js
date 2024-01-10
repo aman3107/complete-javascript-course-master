e85bd5c4abde85e12bec95880af4faf2f22c3460
Aman Mittal <amittal@mirrahealthcare.com>
2024-01-10 23:48:09 +0530

    Create Hold Button functionality

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -6,16 +6,29 @@ const score1El = document.querySelector('#score--1');
 const diceEl = document.querySelector('.dice');
 const btnRoll = document.querySelector('.btn--roll');
 const btnNew = document.querySelector('.btn--new');
-const btnHold = document.querySelector('.btn-hold');
+const btnHold = document.querySelector('.btn--hold');
 const current0El = document.querySelector('#current--0');
 const current1El = document.querySelector('#current--1');
-let currentScore0 = 0;
-let currentScore1 = 0;
+const playerActive0El = document.querySelector('.player--0');
+const playerActive1El = document.querySelector('.player--1');
+// let currentPlayer0 = playerActive0El.classList.contains('player--active');
+let currentScore = 0;
+// let currentScore1 = 0;
+let activePlayer = 0;
+const scores = [0, 0];
 
 // Initial value of both players
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+  playerActive0El.classList.toggle('player--active');
+  playerActive1El.classList.toggle('player--active');
+};
 
 // Rolling dice Functionality
 btnRoll.addEventListener('click', function () {
@@ -29,9 +42,51 @@ btnRoll.addEventListener('click', function () {
   // Check for rolled 1
   if (diceNumber !== 1) {
     // Add to the current score
-    currentScore0 += diceNumber;
-    current0El.textContent = currentScore0;
+    // if (currentPlayer0 === true) {
+    //   currentScore0 += diceNumber;
+    //   current0El.textContent = currentScore0;
+    // } else {
+    //   currentScore1 += diceNumber;
+    //   current1El.textContent = currentScore1;
+    // }
+    currentScore += diceNumber;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     // Switch to next player
+    // document.getElementById(`current--${activePlayer}`).textContent = 0;
+    // activePlayer = activePlayer === 0 ? 1 : 0;
+    // currentScore = 0;
+    // playerActive0El.classList.toggle('player--active');
+    // playerActive1El.classList.toggle('player--active');
+    // if (playerActive0El.classList.contains('player--active')) {
+    //   playerActive0El.classList.remove('player--active');
+    //   playerActive1El.classList.add('player--active');
+    //   currentPlayer0 = false;
+    // } else if (playerActive1El.classList.contains('player--active')) {
+    //   playerActive1El.classList.remove('player--active');
+    //   playerActive0El.classList.add('player--active');
+    //   currentPlayer0 = true;
+    // }
+    switchPlayer();
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  scores[activePlayer] += currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+  if (scores[activePlayer] >= 10) {
+    diceEl.classList.add('hidden');
+    btnRoll.disabled = true;
+    btnHold.disabled = true;
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add('player--winner');
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove('player--active');
+  } else {
+    switchPlayer();
   }
 });
