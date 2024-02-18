9ce47d4f2665929067f9b797a9e7561647d83c18
Aman Mittal <amanmittalma@gmail.com>
2024-02-18 21:31:59 +0530

    Rest Pattern, Short Circuiting

--- 09-Data-Structures-Operators/starter/Practice.js
+++ 09-Data-Structures-Operators/starter/Practice.js
@@ -34,47 +34,164 @@ const restaurant = {
   }) {
     console.log(time, address, startIndex, mainIndex);
   },
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`Here is your delicious pasta with ${ing1},${ing2},${ing3}`);
+  },
+  orderPizza: function (mainIngredient, ...otherIngredient) {
+    console.log(mainIngredient);
+    console.log(otherIngredient);
+  },
 };
 
-restaurant.orderDelivery({
-  time: '22:30',
-  address: 'Via al sole 2',
-  mainIndex: 2,
-  startIndex: 3,
-});
+/* Short Circuting (&& and ||) */
+console.log('--------------OR --------------');
+console.log(3 || 'Jonas');
+console.log('' || 'Jonas');
+console.log(true || 0);
+console.log(undefined || null);
+console.log(undefined || null || 0 || '' || 'Hello' || true || 1);
 
-restaurant.orderDelivery({
-  address: 'del monto',
-  mainIndex: 2,
-});
-/* Destructuring Objects */
-const { name, openingHours, categories } = restaurant;
-console.log(name, openingHours, categories);
+restaurant.numGuest = 23;
+const guest1 = restaurant.numGuest ? restaurant.numGuest : 10;
+console.log(guest1);
 
-const {
-  name: restaurantaName,
-  openingHours: hours,
-  categories: tags,
-} = restaurant;
-console.log(restaurantaName, hours, tags);
+const guest2 = restaurant.numGuest || 10;
+console.log(guest2);
+console.log('-------------- AND --------------');
 
-// Default Values
-const { menu = [], starterMenu: starters = [] } = restaurant;
-console.log(menu, starters);
+console.log(0 && 'Jonas');
+console.log(7 && 'Jonas');
+console.log('Hello' && 23 && null && 'Jonas');
 
-// mutating
-let a = 110;
-let b = 119;
-const obj = { a: 23, b: 20, c: 10 };
-({ b, a } = obj);
-console.log(a, b);
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('mushrooms', 'spinach');
+}
 
-// Nested Objects
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
 
-const {
-  fri: { open: o, close: c },
-} = openingHours;
-console.log(o, c);
+/* Rest Pattern and Parameters */
+
+// Spread, because on right side of =
+// const arr = [1, 2, ...[3, 4]];
+
+// Rest, because on left side of =
+
+// const [a, b, ...others] = [1, 2, 3, 4, 5];
+// console.log(a, b, others);
+
+// const [pizza, , Risotto, ...otherFood] = [
+//   ...restaurant.mainMenu,
+//   ...restaurant.starterMenu,
+// ];
+
+// console.log(pizza, Risotto, otherFood);
+
+/* Objects Rest */
+// const { sat, ...weekDays } = restaurant.openingHours;
+// console.log(sat, weekDays);
+
+/* Functions Rest */
+
+// const add = function (...numbers) {
+//   let sum = 0;
+//   for (let i = 0; i < numbers.length; i++) {
+//     sum = sum + numbers[i];
+//   }
+//   console.log(sum);
+// };
+
+// add(2, 3);
+// add(5, 6, 7);
+// add(1, 2, 3, 4, 5, 6, 7);
+
+// const x = [1, 2, 4];
+// add(...x);
+// restaurant.orderPizza('mushroom', 'onion', 'tomato', 'capsicum');
+
+/* Spread Operator */
+
+// const arr = [7, 8, 9];
+// const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+// console.log(badNewArr);
+// const newArr = [1, 2, ...arr];
+// console.log(newArr);
+// console.log(...newArr);
+// const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+// console.log(newMenu);
+
+// Copy Array
+
+// const mainMenuCopy = [...restaurant.mainMenu];
+// console.log(mainMenuCopy);
+
+// Join 2 or more Arrays
+
+// const arr1 = [1, 2, 3];
+// const arr2 = [4, 5, 6];
+// const arr3 = [...arr1, ...arr2];
+// console.log(arr3);
+
+// const str = 'Aman';
+// const letters = [...str, 'M'];
+// console.log(letters);
+
+// const ingredients = [
+//   prompt("Let's makes pasta! Ingredient 1?"),
+//   prompt('Ingredient 2?'),
+//   prompt('Ingredient 3'),
+// ];
+
+// restaurant.orderPasta(...ingredients);
+
+// Objects
+
+// const newRestaurant = { founder: 'Guisippe', ...restaurant, founded: 1999 };
+// console.log(newRestaurant);
+
+// const restaurantCopy = { ...restaurant };
+// restaurantCopy.name = 'Son of Swad';
+// console.log(restaurant.name);
+// console.log(restaurantCopy.name);
+// restaurant.orderDelivery({
+//   time: '22:30',
+//   address: 'Via al sole 2',
+//   mainIndex: 2,
+//   startIndex: 3,
+// });
+
+// restaurant.orderDelivery({
+//   address: 'del monto',
+//   mainIndex: 2,
+// });
+
+// /* Destructuring Objects */
+// const { name, openingHours, categories } = restaurant;
+// console.log(name, openingHours, categories);
+
+// const {
+//   name: restaurantaName,
+//   openingHours: hours,
+//   categories: tags,
+// } = restaurant;
+// console.log(restaurantaName, hours, tags);
+
+// // Default Values
+// const { menu = [], starterMenu: starters = [] } = restaurant;
+// console.log(menu, starters);
+
+// // mutating
+// let a = 110;
+// let b = 119;
+// const obj = { a: 23, b: 20, c: 10 };
+// ({ b, a } = obj);
+// console.log(a, b);
+
+// // Nested Objects
+
+// const {
+//   fri: { open: o, close: c },
+// } = openingHours;
+// console.log(o, c);
 
 // console.log(restaurant['name']);
 /* Destructuring Arrays */
