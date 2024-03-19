c2be798a1e758886fc27c1e564836741f0933283
Aman Mittal <amanmittalma@gmail.com>
2024-03-19 22:30:03 +0530

    Closures

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,11 +1,74 @@
 'use strict';
 
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+  document.body.addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
+
+/* More Closure Examples */
+
+// Example 1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+g();
+f();
+console.dir(f);
+h();
+f();
+console.dir(f);
+
+// Example 2
+
+const boardPassengrs = function (n, wait) {
+  const perGroup = n / 3;
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+const perGroup = 123;
+boardPassengrs(180, 3);
+
+/* Closures */
+
+// console.log('Hello World');
+// const secureBooking = function () {
+//   let passengerCount = 0;
+//   return function () {
+//     passengerCount++;
+//     console.log(`${passengerCount} passengers`);
+//   };
+// };
+
+// const booker = secureBooking();
+// booker();
+// booker();
+// booker();
+
+// console.dir(booker);
 /* Immediately Invoked Function Expression */
 
-(function () {
-  console.log(`This will never run again`);
-})();
-(() => console.log(`This will never run again`))();
+// (function () {
+//   console.log(`This will never run again`);
+// })();
+// (() => console.log(`This will never run again`))();
 
 /* The Call and Apply Methods */
 
