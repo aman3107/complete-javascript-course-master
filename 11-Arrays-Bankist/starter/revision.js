90924050d67e720b2cdc13b5b4a6e1390c3ec1d0
Aman Mittal <amanmittalma@gmail.com>
2024-04-04 22:19:36 +0530

    Revision_Array

---
+++ 11-Arrays-Bankist/starter/revision.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Simple Array Methods
+
+const arr = ['a', 'b', 'c', 'd', 'e'];
+
+// SLICE
+console.log(arr.slice(1));
+console.log(arr);
+console.log(arr.slice(-1));
+console.log(arr.slice());
+console.log([...arr]);
+
+// SPLICE
+// console.log(arr.splice(1));
+// console.log(arr.splice(1, 2));
+console.log(arr.splice(-1));
+console.log(arr);
